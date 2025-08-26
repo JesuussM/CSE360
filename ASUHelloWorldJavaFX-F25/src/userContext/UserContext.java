67df8ed94aafaf7d49e02057bf5c550e814e5787
Jesus <jesusmirandaa55@gmail.com>
2025-08-26 09:57:55 -0700

    Create static user object

--- ASUHelloWorldJavaFX-F25/src/userContext/UserContext.java
+++ ASUHelloWorldJavaFX-F25/src/userContext/UserContext.java
@@ -6,14 +6,25 @@ public class UserContext {
 	public String Username;
 	public List<String> Roles;
 	
+	// This works for single user since static variables are not instances
+	// If project needs multiuser at once this should change
+	public static UserContext User = new UserContext("Jesus", List.of("User", "Admin"));
+	
 	public UserContext(String name, List<String> roles)
 	{
 		this.Username = name;
 		this.Roles = roles;
 	}
 	
-	// Can be placed here to be used universally in other pages
-	public static boolean HasAdminAccess(UserContext user) {
-		return (user.Roles.contains("Admin"));
+	public boolean HasAdminAccess()
+	{
+		return (Roles != null && 
+				Roles.contains("Admin"));
+	}
+
+	public boolean HasUserAccess() {
+		
+		return (Roles != null && 
+				Roles.contains("User"));
 	}
 }
